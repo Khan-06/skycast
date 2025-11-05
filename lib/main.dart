87e2d7da191e785d6a6be3fbe2b02b6b1d091c19
Khan-06 <m.abdulbasit.k.06@gmail.com>
2025-11-06 01:35:47 +0500

    Working on UI!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:skycast/screens/HomeScreen.dart';
+import 'package:skycast/screens/homeScreen.dart';
 
 void main() {
   runApp(const MyApp());
