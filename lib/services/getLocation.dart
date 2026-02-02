a84aaf08aae676b4bb06975fdafb195cb10e9fe5
Khan-06 <m.abdulbasit.k.06@gmail.com>
2026-02-03 01:27:07 +0500

    Adding Geolocator Functionality!

--- lib/services/getLocation.dart
+++ lib/services/getLocation.dart
@@ -1,4 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
+class Getlocation {
+  double? longitude;
+  double? latitude;
+ static const LocationSettings locationSettings = LocationSettings(accuracy: LocationAccuracy.low, distanceFilter: 100);
 
+ Future<void> getLocation() async {
+   try{
+     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+     if(!serviceEnabled){
+       return Future.error("Location services are disabled!");
+     }
+
+     LocationPermission permission = await Geolocator.checkPermission();
+     if(permission == LocationPermission.denied) {
+       permission = await Geolocator.requestPermission();
+       if(permission == LocationPermission.denied){
+         return Future.error("Permission Denied!");
+       }
+     }
+
+     if(permission == LocationPermission.deniedForever){
+       return Future.error("Permission Denied Forever!");
+     }
+     Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+     latitude = position.latitude;
+     longitude = position.longitude;
+   } catch (e){
+     print("Error in GetLocation $e");
+   }
+ }
+}
